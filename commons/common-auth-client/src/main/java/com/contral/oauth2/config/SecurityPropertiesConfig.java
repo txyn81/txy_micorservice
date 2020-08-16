ad6c6cbe4e5eae3b45a88a5ac81ef1647f279c52
tangxiyuan <txyn81@outlook.com>
2020-08-16 13:14:13 +0800

    feat 完善认证授权组件的最后一步，准备接入认证授权组件

---
+++ commons/common-auth-client/src/main/java/com/contral/oauth2/config/SecurityPropertiesConfig.java
@@ -0,0 +1,13 @@
+package com.contral.oauth2.config;
+
+import com.contral.oauth2.properties.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * @description: TODO
+ * @author: oren.tang
+ * @date: 2020/8/16 1:02 下午
+ */
+@EnableConfigurationProperties(SecurityProperties.class)
+public class SecurityPropertiesConfig {
+}
